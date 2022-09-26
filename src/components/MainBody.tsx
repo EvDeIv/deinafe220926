e6ae143b14fc8b291d5b0f77e1a9c24fd16e994e
Yevhenii Deina <deinak1996@gmail.com>
2022-09-26 19:55:07 +0300

    set color

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -9,8 +9,7 @@ export const MainBody: FC<IMainBody> = ({ imageSrc, handleRandom }) => {
 	const [color, setColor] = useState('red');
 
 	const handleClick = () => {
-		let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
+		let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 		setColor(randomColor);
 		handleRandom();
 	};
